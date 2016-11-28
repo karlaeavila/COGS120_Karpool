38ff24188d63a6472f59d4762309db336a300679
Jimmy Martinez <jim017@ucsd.edu>
2016-11-28 15:58:04 -0800

    added filter with json

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var myaccount = require('./routes/myaccount');
+
+var filter = require('./routes/filter');
+
+//var results = require('./static/filter.html');
 // Example route
 // var user = require('./routes/user');
 
@@ -35,9 +40,18 @@ if ('development' == app.get('env')) {
 }
 
 // Add routes here
-app.get('/', index.view);
+//app.get('/', index.view);
+
 // Example route
 // app.get('/users', user.list);
+app.get('/',filter.view);
+ 	
+//app.get('/', myaccount.view);
+
+
+
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
